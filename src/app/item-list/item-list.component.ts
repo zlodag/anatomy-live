ccb99ce1b096bd61f5ad264c8c194afce626db20
zlodag <11688686+zlodag@users.noreply.github.com>
2018-03-03 10:17:38 +1300

    fixed itemlist

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,37 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { EditStateService } from '../edit-state.service';
-// import {  } from '../models';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html'
 })
-export class ItemListComponent implements OnInit {
+export class ItemListComponent implements OnDestroy {
 
   private itemList = this.db.list(this.db.database.ref('items').child(this.route.snapshot.paramMap.get('userId')).child(this.route.snapshot.paramMap.get('regionId')), ref => ref.orderByKey());
-  
-  items = this.itemList.snapshotChanges().map(actions => actions.map(a => ({
+  public items = [];
+  private sub = this.itemList.snapshotChanges().map(actions => actions.map(a => ({
     key: a.key,
     name: a.payload.val(),
-  })));
+  }))).subscribe(items => {
+    this.items = items;
+  });
+  
+  constructor(public editState: EditStateService, public route: ActivatedRoute, private readonly db: AngularFireDatabase) { }
 
-  constructor(public route: ActivatedRoute, public editState: EditStateService, private readonly db: AngularFireDatabase) { }
-
-  ngOnInit() {
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
-  newItem(newEntry: string) {
+  add(newEntry: string) {
     this.itemList.push(newEntry);
   }
 
-  deleteItem(itemId: string) {
-    this.itemList.remove(itemId).catch(error => {
+  update(itemKey: string, name: string){
+    this.itemList.set(itemKey, name);
+  }
+
+  delete(itemKey: string) {
+    this.itemList.remove(itemKey).catch(error => {
       if (error.code == 'PERMISSION_DENIED') {
         alert('Item is not empty');
       }
     });
   }
+
 }
--- src/app/region-list/region-list.component.ts
+++ src/app/region-list/region-list.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase';
 import { EditStateService } from '../edit-state.service';
-import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-region-list',
   templateUrl: './region-list.component.html',
   styleUrls: ['./region-list.component.css']
 })
-export class RegionListComponent implements OnInit, OnDestroy {
-
+export class RegionListComponent implements OnDestroy {
 
   private regionList = this.db.list(this.db.database.ref('regions').child(this.route.snapshot.paramMap.get('userId')), ref => ref.orderByKey());
   public regions = [];
@@ -25,23 +22,12 @@ export class RegionListComponent implements OnInit, OnDestroy {
   
   constructor(public editState: EditStateService, public route: ActivatedRoute, private readonly db: AngularFireDatabase) { }
 
-  ngOnInit() {
-    // this.db.list()
-    // this.userDoc = this.afs.collection('users').doc(this.route.snapshot.paramMap.get('userId'));
-    // this.regionsCollection = this.userDoc.collection<Region>('/regions', ref => ref.orderBy('ts'));
-    // this.regions = this.regionsCollection.snapshotChanges().map(actions => actions.map(a => a.payload.doc.id));
-  }
-
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
 
   add(newEntry: string) {
     this.regionList.push(newEntry);
-    // this.regionList.
-    // this.regionsCollection.doc<Region>(newEntry).set({
-    //   ts: firebase.firestore.FieldValue.serverTimestamp(),
-    // });
   }
 
   update(regionKey: string, name: string){
