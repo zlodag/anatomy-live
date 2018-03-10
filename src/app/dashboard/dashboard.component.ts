7b17f6458499a91dc77f58f7cd14044b8e543fbe
zlodag <11688686+zlodag@users.noreply.github.com>
2018-03-10 15:39:38 +1300

    added backup function

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http, Headers } from '@angular/http';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { OwnerService } from '../owner.service';
+import { saveAs } from 'file-saver/FileSaver';
+import 'rxjs/add/operator/first';
+import { database } from 'firebase';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +14,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DashboardComponent {
 
-  constructor(public route: ActivatedRoute) { }
+  constructor(
+    private readonly db: AngularFireDatabase,
+    private auth: AngularFireAuth,
+    public route: ActivatedRoute,
+    public ownerService: OwnerService
+    ) { }
 
+  backup() {
+    this.auth.authState.first().subscribe(user => {
+      if (user) {
+        Promise.all<database.DataSnapshot, database.DataSnapshot, database.DataSnapshot, database.DataSnapshot>([
+          this.db.database.ref('regions').child(user.uid).once('value'),
+          this.db.database.ref('items').child(user.uid).once('value'),
+          this.db.database.ref('details').child(user.uid).once('value'),
+          this.db.database.ref('images').child(user.uid).once('value'),
+        ]).then(([regions, items, details, images]) => {
+            const json = JSON.stringify({
+              regions: regions.val(),
+              items: items.val(),
+              details: details.val(),
+              images: images.val(),
+            });
+            const blob = new Blob([json], { type: 'application/json' });
+            const now = new Date();
+            saveAs(blob, `backup-${now.toISOString()}.json`);
+        });
+      }
+    });
+  }
 }
